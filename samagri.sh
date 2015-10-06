67c2afbdfc962334925861289bdd4a73cf3bfe13
Jaison Justus <jaison.justus.lp@gmail.com>
2015-10-06 14:17:43 +0530

    samagri install script

---
+++ samagri.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Hello, " + $USER
+echo "- creating directory"
+mkdir samagri
+cd samagri
+
+echo "- getting Samagri/Pallete.."
+wget -q . https://raw.githubusercontent.com/27AE60/samagri/master/pallete.scss
+
+echo "- getting Samagri/Typography.."
+wget -q . https://raw.githubusercontent.com/27AE60/samagri/master/typography.scss
+
+echo "- getting Samagri/Viewport.."
+wget -q . https://raw.githubusercontent.com/27AE60/samagri/master/viewport.scss
+
+echo "Done!"
